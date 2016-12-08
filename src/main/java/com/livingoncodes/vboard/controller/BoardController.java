bd37ab5fc41c3e41dd33026cb535bbdea7aede1a
David Birtwell <David Birtwell>
2016-12-08 17:21:48 -0500

    wire BoardRepository to controller

--- src/main/java/com/livingoncodes/vboard/controller/BoardController.java
+++ src/main/java/com/livingoncodes/vboard/controller/BoardController.java
@@ -2,11 +2,15 @@ package com.livingoncodes.vboard.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.livingoncodes.vboard.entity.Board;
+import com.livingoncodes.vboard.repository.BoardRepository;
 
 @Controller
 public class BoardController {
@@ -14,14 +18,24 @@ public class BoardController {
 	private static final Logger LOGGER = LoggerFactory
 			.getLogger(BoardController.class);
 
+	// Autowire repository
+	@Autowired
+	private BoardRepository boardRepository;
 	
 	@RequestMapping(value = "/{boardName}", method = RequestMethod.GET)
 	public String showBoard(@PathVariable("boardName") String boardName, Model model) {
 		
 		LOGGER.debug("Rendering view for board: " + boardName);
 		
-		// bind the board name to the model
+		Board board = boardRepository.findByName(boardName);
+		if(board == null) {
+			LOGGER.debug("Board not found: " + boardName);
+			return "newboard";
+		}
+			
+		// bind board attributes to the model
 		model.addAttribute("boardName", boardName);
+		model.addAttribute("boardContent", board.getContent());
 		
 		// renders board.html
 		return "board";
